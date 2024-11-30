f97db6910f1dd163fa6bafc50a4dc2f85ff2ee65
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-30 21:13:13 +0530

    created product template

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,8 @@ const App = () => {
                 <div className="card mr-3 mb-3  p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center">
                     <div className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
                          style={{backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)"}}></div>
-                    <h1>erjk afjkdj jlafjs jld dfsjlf</h1>
+                    <h1 className="hover:text-blue-300">Elegante Leather Back Pack</h1>
                 </div>
-
-
             </div>
         </div>
 
