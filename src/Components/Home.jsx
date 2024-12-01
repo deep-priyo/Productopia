f7ded82647310c8e4c45f7ca38b78efc95f7d599
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-01 23:26:48 +0530

    dynamic handling of products

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,18 +6,22 @@ import Loading from "./Loading.jsx";
 
 const Home = () => {
     const [products]= useContext(ProductContext);
+    console.log(products);
     return ( products? (
         <>
 
             <Nav/>
             <div className=" w-[85%]  p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
+                {products.map((product) => (
 
-                <Link to="/details/1"
-                    className="card mr-3 mb-3  p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center">
-                    <div className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-                         style={{backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)"}}></div>
-                    <h1 className="hover:text-blue-300">Elegante Leather Back Pack</h1>
-                </Link>
+                    <Link to={"/details/" + product.id}
+                          className="card mr-3 mb-3  p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center">
+                        <div className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
+                             style={{backgroundImage: `url(${product.image})`}}></div>
+                        <h1 className="hover:text-blue-300">{product.title}</h1>
+                    </Link>
+                ))}
+
             </div>
         </>
 
