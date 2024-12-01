b1dbbbace7e5b2112123005a6891da119011510b
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-01 17:38:19 +0530

    Implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
 import React from 'react'
+import {Route, Routes} from "react-router-dom";
+import Home from "./Components/Home.jsx";
+import Details from "./Components/Details.jsx";
 
 const App = () => {
     return (
         <div className="h-screen w-screen flex ">
-            <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5">
-                <a className="py-3 px-5 border border-blue-100 rounded text-blue-300
-                " href="/create">Add new Product</a>
-                <hr className="w-[80%] my-3"/>
-                <h1 className="text-2xl  mb-3 w-[80%]">Category Filter</h1>
-                <ul className="  w-[80%]">
-                    <li className="mb-3 flex items-center">
-                        <span className=" w-[15px] h-[15px] mr-2  bg-blue-100 rounded-full"></span>Cat 1
-                    </li>
-                    <li className="mb-3 flex items-center">
-                        <span className=" w-[15px] h-[15px] mr-2  bg-red-100 rounded-full"></span>Cat 2
-                    </li>
-                    <li className="mb-3 flex items-center">
-                        <span className=" w-[15px] h-[15px] mr-2  bg-green-100 rounded-full"></span>Cat 3
-                    </li>
-                    <li className="mb-3 flex items-center">
-                        <span className=" w-[15px] h-[15px] mr-2  bg-amber-100 rounded-full"></span>Cat 4                    </li>
-                </ul>
-            </nav>
-            <div className=" w-[85%]  p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/details/:id" element={<Details/>} />
+            </Routes>
 
-                <div className="card mr-3 mb-3  p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center">
-                    <div className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-                         style={{backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)"}}></div>
-                    <h1 className="hover:text-blue-300">Elegante Leather Back Pack</h1>
-                </div>
-            </div>
         </div>
 
     )
