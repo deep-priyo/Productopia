da9b01e25ac1eb673e4617284be234a865a62a79
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-01 17:57:21 +0530

    Wrap with Context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
+import Context from "./Utils/Context.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Context>
+    <BrowserRouter>
       <App />
-  </BrowserRouter>
-
+    </BrowserRouter>
+  </Context>
 
 )
