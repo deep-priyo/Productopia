fbd9f232eeb41c6aa004e4836b80dcabc5d2634f
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-02 00:43:34 +0530

    Now details has back button

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -24,7 +24,10 @@ const Details = () => {
     return ( product?(
             <>
 
-
+                <button onClick={gobackHandler}
+                        className="absolute w-8 h-8 rounded-full my-[20vh] mx-[178vh] flex justify-center items-center">
+                    <img src="/src/assets/img.png" alt=""/>
+                </button>
 
                 <div className="w-[70%] flex h-full justify-between items-center m-auto p-[10%]   ">
 
@@ -37,15 +40,18 @@ const Details = () => {
 
                         <h1 className="text-4xl">
                             {product.title}
-                </h1>
-                <h3 className="text-zinc-400 my-5 ">{product.category}</h3>
-                <h2 className="text-red-300 mb-3">Rs. {product.price}</h2>
-                <p className="mb-5">{product.description}</p>
+                        </h1>
+                        <h3 className="text-zinc-400 my-5 ">{product.category}</h3>
+                        <h2 className="text-red-300 mb-3">Rs. {product.price}</h2>
+                        <p className="mb-5">{product.description}</p>
 
-                <Link className="py-2 px-5 border rounded text-blue-400 border-blue-200 mr-3 hover:bg-blue-100 hover:text-blue-600" >Edit</Link>
-                <Link className="py-2 px-5 border rounded text-red-400 border-red-200 mr-3 hover:bg-red-100 hover:text-red-600">Delete</Link>
-            </div>
-        </div></>):<Loading/>
+                        <Link
+                            className="py-2 px-5 border rounded text-blue-400 border-blue-200 mr-3 hover:bg-blue-100 hover:text-blue-600">Edit</Link>
+                        <Link
+                            className="py-2 px-5 border rounded text-red-400 border-red-200 mr-3 hover:bg-red-100 hover:text-red-600">Delete</Link>
+                    </div>
+                </div>
+            </>) : <Loading/>
     )
 }
 export default Details
